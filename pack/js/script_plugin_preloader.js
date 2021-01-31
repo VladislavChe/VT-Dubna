91186a579c69212433a198a862b670964aaf9fc8
che_vladislav <cheremisin0610@mail.ru>
2021-01-31 11:28:11 +0300

    start

--- pack/js/script_plugin_preloader.js
+++ pack/js/script_plugin_preloader.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-    $(".dws-progress-bar").circularProgress({
-        color: "#25ffe4",
-        line_width: 17,
-        height: "350px",
-        width: "350px",
-        percent: 0,
-        // counter_clockwise: true,
-        starting_position: 25
-    }).circularProgress('animate', 100, 2000);
+  $('.dws-progress-bar')
+    .circularProgress({
+      color: '#008496', //цвет прогресс бара
+      line_width: 10, //толщина линии прогресс бара
+      height: '250px', //высота прогресс бара
+      width: '250px', //ширина прогресс бара
+      percent: 0,
+      // counter_clockwise: true,
+      starting_position: 25,
+    })
+    .circularProgress('animate', 100, 4500); //продолжительность анимации
 });
 
 $(window).on('load', function () {
-   var $preloader = $('#preloader');
-   $preloader.delay(1800).fadeOut('slow');
+  var $preloader = $('#preloader');
+  $preloader.delay(4800).fadeOut('slow'); //до выключения прелоадера
 });
